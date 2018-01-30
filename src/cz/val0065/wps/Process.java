dbcc087902bccfd0aef21fbcb83b964254d7a48b
val0065 <david.vala@valada.cz>
2018-01-30 15:25:23 +0100

    TODO

--- src/cz/val0065/wps/Process.java
+++ src/cz/val0065/wps/Process.java
@@ -6,5 +6,9 @@ package cz.val0065.wps;
  */
 
 public class Process {
+
+    String overlay(String point, double distance) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
