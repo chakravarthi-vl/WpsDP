26b0d6213bf0fb4a8bf923cb641253b2cfe2a30e
val0065 <david.vala@valada.cz>
2018-01-30 15:06:25 +0100

    TODO

---
+++ src/cz/val0065/wps/CalculateLength.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.val0065.wps;
+
+/**
+ *
+ * @author David
+ * Calculate length of lines inside shapefile file.
+ * WPS development testing.
+ */
+public class CalculateLength {
+    
+}
