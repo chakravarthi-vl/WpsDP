cc9da399b30017dca38241ee08542f19e517466c
val0065 <david.vala@valada.cz>
2018-03-03 10:31:58 +0100

    Progress

--- src/cz/val0065/wps/Main.java
+++ src/cz/val0065/wps/Main.java
@@ -1,33 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.val0065.wps;
 
 import java.io.IOException;
 
-/**
- *
- * @author David
- */
 public class Main {
     
         public static void main(String[] args) throws IOException, Exception {
-        // TODO code application logic here
-        Process e = new Process();
-        //System.out.println(e.search("600000 4920000", 10000.0d));
-        //System.out.println(e.overlay("600000 4920000", 10000.0d));
-        //System.out.println(e.bbox("M"));
-        //System.out.println(e.wfs("sf_restricted"));
-        //System.out.println(e.testREST_PUT_Local());
-        //System.out.println(e.overlayWMS("600000 4920000", 5000.0d));
-        //System.out.println(e.postGIS_example1());
-        //System.out.println(e.postGIS_example2("600000 4920000", 5000.0d));
-        //System.out.println(e.networking("Ostrava", "Fulnek"));
-        //System.out.println(e.overlayCollectionOutput("600000 4920000", 5000.0d));
-        
-        e.length();
+
+        Process proc = new Process();
+
+        System.out.println(proc.overlay("600000 4920000", 10000.0d));
     }
     
 }
